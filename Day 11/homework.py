e5786175444be1291efa2ed688be06a1a1acc2f0
GegaGogoladze <gogoladzegega4@gmail.com>
2024-04-14 20:55:16 +0400

    commit

--- Day 11/homework.py
+++ Day 11/homework.py
@@ -1,40 +1,40 @@
 #დავალება 2
-# print(10<11)
-# print(10<51)
-# print(19<20)
+print(10<11)
+print(10<51)
+print(19<20)
 
 
-# print(92>93)
-# print(98>5)
-# print(95>10)
+print(92>93)
+print(98>5)
+print(95>10)
 
 
-# print(5<=6)
+print(5<=6)
 
 
-# print(87>=77)
+print(87>=77)
 
 
-# print(42==2)
+print(42==2)
 
 
-# print(21!=0)
+print(21!=0)
 
 
 #დავალება 3
-# print(22>22 and 90>55)
-# print(11>85 and 30>55)
-# print(44>33 and 33>58)
-# print(43>38 and 37>55)
-# print(91>44 and 65<44)
+print(22>22 and 90>55)
+print(11>85 and 30>55)
+print(44>33 and 33>58)
+print(43>38 and 37>55)
+print(91>44 and 65<44)
 
 
 
-# print(11>392 and 33>550)
-# print(44>331 and 33>551)
-# print(111>308 and 33>552)
-# print(13>350 and 33>550)
-# print(199>358 and 33>56)
+print(11>392 and 33>550)
+print(44>331 and 33>551)
+print(111>308 and 33>552)
+print(13>350 and 33>550)
+print(199>358 and 33>56)
 
 
 #დავალება 4
